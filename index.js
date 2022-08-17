ea1ab2336308892f0c9a580b56fc9a4e21996594
gibsonbett <gibson.bett@student.moringaschool.com>
2022-08-17 20:13:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser= "Gibson Bett";
+console.log(currentUser.length)
+currentUser= "GIBSON BETT"
+let welcomeMessage= `Welcome to Flatbook, ${currentUser}, !`;
+let excitedWelcomeMessage= "WELCOME TO FLATBOOK, " + currentUser + "!";
+console.log(excitedWelcomeMessage)
+
+let shortGreeting= `Welcome, ${currentUser.slice(0,1)}, !`
+console.log(shortGreeting)
